cd539f32c97dfc0fe436a05e2bf053859e0c0aa6
Munk-V <s240335@dtu.dk>
2026-01-14 16:11:44 +0100

    Added all sensors to sharedState

--- sharedState.h
+++ sharedState.h
@@ -5,16 +5,17 @@
  * Shared system state
  * Sensor modules write to their own variables
  * Other modules can read from it
-*/ 
+ */
+struct SharedState {
+  // DHT11: Temperature and humidity
+  float tempC = NAN;
+  float humidityPct = NAN;
+  bool  hasDht = false;
 
-struct SharedState{
-    // DHT11: Temperature and humidity
-    float tempC = NAN;
-    float humidityPct = NAN;
-    bool hasDht = false;
-    
-    // Light sensor
-    float light = NAN;
-    bool lampOn = false;
+  // Light module (hours of light received today + lamp state)
+  float lightHoursToday = NAN;
+  bool  lampOn = false;
 
+  // Heater module (heat lamp indicator)
+  bool  heaterOn = false;
 };
