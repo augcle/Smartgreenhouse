8b6371f059e074051344d9813acfe3ae78b9510d
Munk-V <s240335@dtu.dk>
2026-01-14 16:17:44 +0100

    Added all sensors to main

--- Smartgreenhouse.ino
+++ Smartgreenhouse.ino
@@ -15,32 +15,41 @@ void setup() {
   dht11Begin();
   misterInit();
   lightInit();
+  heaterBegin();
 }
 
 void loop() {
-  unsigned long now = millis();
-  if (now - lastReading >= READING_PERIOD){
-    lastReading = now;
-    // Important dht11 reading
-    dht11Read(state);
-    
-    // Optional dht print statements
-    if (state.hasDht) {
-      Serial.print("Temp: ");
-      Serial.print(state.tempC);
-      Serial.print("  Hum: ");
-      Serial.println(state.humidityPct);
-    } else {
-      Serial.println("DHT read failed");
-  }
-  }
+  // 1) Read sensors
+  dht11Read(state);
+
+  // 2) Run control modules
   lightUpdate(state);
+  heaterUpdate(state);
+
+  // 3) (Optional) mister control (leave OFF for now)
+  // misterState(false);
+
+  // 4) Debug print
   static uint32_t lastPrint = 0;
-  if (millis() - lastPrint > 1000) {
+  if (millis() - lastPrint >= PRINT_MS) {
     lastPrint = millis();
-    Serial.print("hoursToday=");
+
+    Serial.print("hasDht=");
+    Serial.print(state.hasDht ? "true" : "false");
+
+    Serial.print(" tempC=");
+    Serial.print(state.tempC, 1);
+
+    Serial.print(" hum=");
+    Serial.print(state.humidityPct, 1);
+
+    Serial.print(" lightHoursToday=");
     Serial.print(state.lightHoursToday, 2);
+
     Serial.print(" lampOn=");
-    Serial.println(state.lampOn ? "true" : "false");
+    Serial.print(state.lampOn ? "true" : "false");
+
+    Serial.print(" heaterOn=");
+    Serial.println(state.heaterOn ? "true" : "false");
   }
 }
