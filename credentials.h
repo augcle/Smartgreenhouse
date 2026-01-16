2877f82a76869c986ec1e3a2eaca20aceecb81ea
HyberDuck <254696443+HyberDuck@users.noreply.github.com>
2026-01-16 22:09:55 +0100

    credentials.h add

---
+++ credentials.h
@@ -0,0 +1,13 @@
+/// Purpose: Store Wi-Fi credentials and ThingSpeak API keys.
+// DO NOT commit this to GitHub – add it to .gitignore, when uploading.
+
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+const unsigned long channelID = ;
+const char* APIKey = "";
+
+#endif
